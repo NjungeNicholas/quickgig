2e194d55e8c5cfc56646b6685beda4c71f236fe7
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-08-15 21:06:52 +0300

    fix(backend-accounts): update tasker profile creation logic to use get_or_create for better handling

--- quickgig_api/accounts/signals.py
+++ quickgig_api/accounts/signals.py
@@ -7,4 +7,4 @@ from .models import BaseUser, TaskerProfile
 def create_tasker_profile(sender, instance, created, **kwargs):
     # If the user is a tasker but doesn't have a profile, create one
     if instance.is_tasker and not hasattr(instance, 'tasker_profile'):
-        TaskerProfile.objects.create(user=instance)
+        TaskerProfile.objects.get_or_create(user=instance)
