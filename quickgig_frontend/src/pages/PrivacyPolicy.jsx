c6dc67c3a4bdb83b07b9f68ef4a67fafab3a7107
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-08-20 13:17:34 +0300

    feat: add About, Contact, PrivacyPolicy, and TermsOfService pages

---
+++ quickgig_frontend/src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,12 @@
+const PrivacyPolicy = () => {
+    return (
+        <div className="justify-center mt-50 mb-80">
+            <h1 className="font-header text-text text-center text-4xl">Privacy Policy</h1>
+            <p className="text-center font-body text-lg text-gray-800 mt-4">
+                We value your privacy and are committed to protecting your personal information.
+            </p>
+        </div>
+    );
+}
+
+export default PrivacyPolicy;
