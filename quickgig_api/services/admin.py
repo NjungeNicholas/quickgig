8b4d18995c3881b3130f9108a48e12fa8d550b1b
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-08-20 13:45:48 +0300

    feat: enhance admin interface by adding ID to Service and Category list displays

--- quickgig_api/services/admin.py
+++ quickgig_api/services/admin.py
@@ -8,12 +8,12 @@ admin.site.index_title = "Welcome to the QuickGig Admin Portal"
 # Register your models here.
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price')
+    list_display = ('name', 'category', 'price', 'id')
     search_fields = ('name', 'description')
     list_filter = ('category',)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'id')
     search_fields = ('name',)
