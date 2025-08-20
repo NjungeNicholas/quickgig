0a4e9b0222a1c026cc8b48f98ed4bdc8a242997a
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-08-20 13:18:39 +0300

    feat: implement routing with Navbar and Footer, and add main application pages

--- quickgig_frontend/src/App.jsx
+++ quickgig_frontend/src/App.jsx
@@ -1,10 +1,31 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/layouts/Navbar"
+import Footer from "./components/layouts/Footer"
+import Home from "./pages/Home"
+import Services from "./pages/Services"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import PrivacyPolicy from "./pages/PrivacyPolicy"
+import TermsOfService from "./pages/TermsOfService"
 
 function App() {
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          {/* Main routes */}
+          <Route index element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* * */}
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/terms" element={<TermsOfService />} />
+          <Route path="*" element={<h1 className="text-center text-2xl mt-20">Page Not Found</h1>} />
+        </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
