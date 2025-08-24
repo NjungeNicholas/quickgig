e14040ac16c8640022fd595b1647d3ee244a787b
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-08-24 17:25:05 +0300

    feat: integrate Django REST framework and configure JWT authentication

--- quickgig_api/quickgig_api/settings.py
+++ quickgig_api/quickgig_api/settings.py
@@ -43,7 +43,28 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'services',
     'tasks',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
