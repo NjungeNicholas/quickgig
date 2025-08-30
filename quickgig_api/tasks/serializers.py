064dbeb802ea505d3e1f5209e5336f8af1b2c5d2
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-08-30 03:10:25 +0300

    feat(serializers): add task_name field to BookingSerializer for improved task detail representation

--- quickgig_api/tasks/serializers.py
+++ quickgig_api/tasks/serializers.py
@@ -17,13 +17,15 @@ class BookingSerializer(serializers.ModelSerializer):
     client_name = serializers.CharField(source="client.username", read_only=True)
     tasker_name = serializers.CharField(source="tasker.username", read_only=True)
     slot_detail = AvailabilitySlotSerializer(source="availability_slot", read_only=True)
+    task_name = serializers.CharField(source="task.name", read_only=True)
 
     class Meta:
         model = Booking
         fields = [
             "id", "client", "client_name",
             "tasker", "tasker_name",
-            "task", "description",
+            "task", "task_name",
+            "description",
             "availability_slot", "slot_detail",
             "status", "created_at", "updated_at"
         ]
